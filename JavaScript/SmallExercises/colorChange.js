cadadc50eef5a60cad7a1b4ae7e86f67d8660d95
nMinhDev <heomeco2014@gmail.com>
2021-09-18 11:25:10 +0700

    no

--- JavaScript/SmallExercises/colorChange.js
+++ JavaScript/SmallExercises/colorChange.js
@@ -1,6 +1,8 @@
 $ = document.querySelector.bind(document);
 const btn = $('#btn');
 const h1 = $('h1');
+const clickMe = $('input');
+const tweetForm = document.querySelector('#tweetForm');
 
 function changeColor() {
     const r = Math.floor(Math.random() * 255);
@@ -8,7 +10,48 @@ function changeColor() {
     const b = Math.floor(Math.random() * 255);
     return `rgba(${r}, ${g}, ${b})`;
 }
-btn.addEventListener('mouseover', function () {
+btn.addEventListener('click', function () {
     document.body.style.backgroundColor = changeColor();
     h1.innerText = changeColor();
+})
+
+clickMe.addEventListener('keydown', function (e) {
+    console.log(e.code);
+})
+clickMe.addEventListener('keyup', function () {
+    console.log('KeyUp');
+})
+
+const tweetContainer = document.querySelector('#tweetForm');
+tweetForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    e.stopImmediatePropagation();
+    let usernameInput = tweetForm.elements.username;
+    let tweetInput = tweetForm.elements.comment;
+    addTweet(usernameInput.value, tweetInput.value);
+    usernameInput.value = '';
+    tweetInput.value = '';
+})
+
+const addTweet = (username, tweet) => {
+
+    const newTweet = document.createElement('li');
+    const bTag = document.createElement('b');
+    bTag.append(username);
+    newTweet.append(bTag);
+    newTweet.append(`-${tweet}`);
+
+    tweetContainer.append(newTweet);
+
+}
+
+// const lis = document.querySelectorAll('li')
+// for (let li of lis) {
+//     li.addEventListener('click', () => {
+//         li.remove();
+//     })
+// }
+const a = document.querySelector('#tweets');
+a.addEventListener('click', (e) => {
+    e.target.nodeName ==='LI' && e.target.remove();
 })
