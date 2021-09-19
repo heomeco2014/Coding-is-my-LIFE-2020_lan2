f63f89f92506da0de638bf84c69d34a6e0f80d8c
nMinhDev <heomeco2014@gmail.com>
2021-09-19 22:24:02 +0700

    50days

--- 9 50 project 50 days/02 Day 2/loading.js
+++ 9 50 project 50 days/02 Day 2/loading.js
@@ -26,6 +26,7 @@ prev.addEventListener('click', () => {
 function update() {
     circles.forEach((circle, index) => {
         if (index < currentActive) {
+            console.log(currentActive, index);
             circle.classList.add('active');
         } else {
             circle.classList.remove('active');
@@ -35,11 +36,11 @@ function update() {
     const actives = document.querySelectorAll('.active');
     progress.style.width = ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
 
-    if(currentActive ===1){
+    if (currentActive === 1) {
         prev.disabled = true;
-    }else if(currentActive===circles.length){
+    } else if (currentActive === circles.length) {
         next.disabled = true;
-    }else{
+    } else {
         prev.disabled = false;
         next.disabled = false;
     }
